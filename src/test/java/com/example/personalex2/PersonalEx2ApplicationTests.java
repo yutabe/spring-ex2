e2938de3779938969ffd3cae531950644eab5e3e
yutabe <abe21453@cs.saisoncard.co.jp>
2023-06-16 10:01:13 +0900

    a

---
+++ src/test/java/com/example/personalex2/PersonalEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalEx2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
